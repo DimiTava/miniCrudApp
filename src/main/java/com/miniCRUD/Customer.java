261d71248835ae131e2077db597a26d514402350
DimiTava <dimitavanidis@gmail.com>
2018-02-08 15:53:28 +0200

    Initial commit

---
+++ src/main/java/com/miniCRUD/Customer.java
@@ -0,0 +1,97 @@
+package com.miniCRUD;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers", schema = "minicruddata", catalog = "")
+public class Customer {
+    private long id;
+    private String name;
+    private String status;
+    private String email;
+    private String firstName;
+    private String lasttName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+
+
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 45)
+    public String getName() {
+        return firstName+" "+lasttName;
+    }
+
+    public void setName(String name) {
+        this.name = firstName+" "+lasttName;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = false, length = 20)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 45)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer that = (Customer) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/miniCRUD/CustomerStatus.java
@@ -0,0 +1,5 @@
+package com.miniCRUD;
+
+public enum CustomerStatus {
+    ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+}
